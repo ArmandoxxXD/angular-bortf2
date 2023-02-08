d531e9abd5a20bb1cece58b3ed81c4b665ace915
ArmandoxxXD <mamojose58@gmail.com>
2023-02-07 22:50:23 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/components/footer/footer.component.html 	new file:   src/app/components/footer/footer.component.scss 	new file:   src/app/components/footer/footer.component.ts 	new file:   src/app/components/home/home.component.html 	new file:   src/app/components/home/home.component.scss 	new file:   src/app/components/home/home.component.ts 	modified:   src/app/components/login/login.component.html 	modified:   src/app/components/login/login.component.scss 	modified:   src/app/components/login/login.component.ts 	new file:   src/app/components/navbar/navbar.component.html 	new file:   src/app/components/navbar/navbar.component.scss 	new file:   src/app/components/navbar/navbar.component.ts 	modified:   src/app/components/register/register.component.html 	modified:   src/app/components/register/register.component.scss 	modified:   src/app/components/register/register.component.ts 	modified:   src/index.html 	modified:   src/styles.scss

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  userName:String;
+  email:String;
+  password:String;
+  confirmpassword:String;
 
-  constructor() { }
+  constructor() { 
+    this.userName='';
+    this.email='';
+    this.password='';
+    this.confirmpassword='';
+
+    document.body.style.margin = "0";
+    document.body.style.display = "grid";
+    document.body.style.height = "100vh";
+    document.body.style.fontFamily = "Source Code Pro', monospace";
+    document.body.style.background = "linear-gradient(#141e30, #243b55)";
+    document.body.style.marginBottom="100px";
+  }
 
   ngOnInit(): void {
   }
 
+  register(){
+    console.log('UserName : ' +this.userName);
+    console.log('Email: ' +this.email);
+    console.log('Password: '+ this.password);
+    console.log('ConfirmPassword : ' +this.confirmpassword);
+  };
 }
