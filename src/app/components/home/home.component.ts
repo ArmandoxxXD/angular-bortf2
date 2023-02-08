9eca7fa0d712a3167b9f5d3d66bfb05342c016f2
ArmandoxxXD <108047890+ArmandoxxXD@users.noreply.github.com>
2023-02-08 11:01:00 -0600

    1 file modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from "jquery";
+//import * as $ from "jquery";
 //import * as $ from "slick-carousel";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-
   constructor() {
-    document.body.style.margin = "0";
-    document.body.style.display = "grid";
-    document.body.style.height = "100vh";
+    document.body.style.margin = '0';
+    document.body.style.display = 'grid';
+    document.body.style.height = '100vh';
     document.body.style.fontFamily = "Source Code Pro', monospace";
-    document.body.style.background = "linear-gradient(#141e30, #243b55)";
-   }
-
-  ngOnInit(): void {
-    window.location.href="#slide-1";
+    document.body.style.background = 'linear-gradient(#141e30, #243b55)';
   }
 
-
+  ngOnInit(): void {
+    window.location.href = '#slide-1';
+  }
 }
